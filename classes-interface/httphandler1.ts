84a66054faa3d85921c29f6f1c28551690391f31
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2022-02-09 16:27:50 -0500

    updated dev setting on the handler

--- classes-interface/httphandler1.ts
+++ classes-interface/httphandler1.ts
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import { appContext } from "./app-conext";
 
 
 
@@ -7,6 +9,29 @@ export interface HttpHandler1Interface{
 
 
 class http1 implements HttpHandler1Interface{
+    /////////////////////////////////////////////
+    private useURL:string = "http://20.124.74.192:3000";
+    private devMode:boolean = false;
+    private IndexURL =0;
+    private context = useContext(appContext);
+    //constructor
+    constructor(dev:boolean = false, IndexURL=1){
+        this.devMode=dev;
+        this.IndexURL=IndexURL
+    }
+    //////////////////////////////////////////////
+
+    /**this function returns the URL to work with, if devMod is set to false, 
+    * it will return the production URL, if true, it will return 'http//localhost:[port]'*/
+    private getURL(){
+        if(!this.devMode){ return this.useURL} //postman mock
+        switch(this.IndexURL){
+            case 0:{ return ''}
+            case 1:{ return ''}
+        }
+    }
+
+    
 
 }
 
