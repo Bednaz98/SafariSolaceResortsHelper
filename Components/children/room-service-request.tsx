9b1a6b20fce0c55ee515b6b666d36f3965c8852e
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2022-02-10 14:39:47 -0500

    only finished room service

--- Components/children/room-service-request.tsx
+++ Components/children/room-service-request.tsx
@@ -1,13 +1,52 @@
 import React from "react";
 import { View } from "react-native";
+import { Offering, ServiceRequest } from "../../classes-interface/api-entities";
+import BasicButton from "../../SafariSolaceStyleTools/basicbutton";
 import BasicModal from "../../SafariSolaceStyleTools/basicmodal";
+import BasicText from "../../SafariSolaceStyleTools/basictext";
 
 
 
 
 export default function RoomServiceRequest(props){
 
-    return(<View>
-        <BasicModal openTitle ={(props?.openTitle ?? "null open title")} child={props.child}/>
-    </View>)
+    const serviceRequest:ServiceRequest = props.serviceRequest
+
+    function SwitchButtonDisplay(){
+        if(serviceRequest.status == "Ordered" )          return <BasicButton title={' Start Order'} onPress={()=>{}} />
+        else if(serviceRequest.status ==  "Processing" ) return <BasicButton title={' Mark order as complete'} onPress={()=>{}} />
+        else                                             return <></>
+    }
+
+    function SwitchStatusDisplay(){
+        return(<View style={{flexDirection:"row"}}>
+            <BasicText text={"Status"}/><BasicText text={serviceRequest.status}/><SwitchButtonDisplay/>
+        </View>)
+    }
+
+    function GetOfferingString(){
+        let tempString:string= '';
+        for(let i =0; i < serviceRequest.requestedOffering.length; i ++){
+            if(i==0){tempString+= `\t-${serviceRequest.requestedOffering[i].desc}`;}
+            else{tempString+= `\n\t-${serviceRequest.requestedOffering[i].desc}`;}
+        }
+        return tempString
+    }
+
+
+    function RequestDisplay(){
+        const dateDisplay = (new Date(serviceRequest.created))
+        return(
+        <>
+            <BasicText text={`Room: ${serviceRequest.room}`}/>
+            <BasicText text={`Created: ${dateDisplay.toDateString()}, at ${dateDisplay.toLocaleTimeString()}`}/>
+            <View><BasicText text={'Requested Offerings:'}/><BasicText text={GetOfferingString()}/></View>
+            <SwitchStatusDisplay/>
+        </>)
+    }
+
+    return(
+    <>
+        <BasicModal openTitle ={(props?.openTitle ?? "null open title")} child={<RequestDisplay/>}/>
+    </>)
 }
